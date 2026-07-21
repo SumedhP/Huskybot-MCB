5c9df57c2eceff0b63b7c9baa1f0689796bee7d4
Sumedh Panatula <sumedh.panatula@gmail.com>
2026-07-20 19:46:53 -0700

    Agitator subsystem logic

---
+++ huskybot-mcb-project/src/control/agitator/agitator_subsystem.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "tap/algorithms/smooth_pid.hpp"
+#include "tap/control/subsystem.hpp"
+#include "tap/drivers.hpp"
+#include "tap/motor/motor_interface.hpp"
+
+/**
+ * @brief Agitator subsystem controls the agitator motor to a given setpoint increment.
+ *
+ * Takes in:
+ * MotorInterface
+ * Drivers
+ * SmoothPIDConfig
+ * Some config of how much to move and tolerance
+ */
+
+namespace huskybot::control::agitator
+{
+struct AgitatorConfig
+{
+    float setpointIncrement = 0.0f;  /// Angle to rotate per click.
+    float tolerance = 0.0f;          /// The tolerance for the agitator's position.
+};
+
+class AgitatorSubsystem : public tap::control::Subsystem
+{
+public:
+/**
+ * Constructs an AgitatorSubsystem with the given motor, drivers, and PID configuration.
+ * 
+ * @param drivers A pointer to the Drivers instance
+ * @param motor The motor to use
+ * @param config The configuration for the agitator
+ * @param pidConfig The configuration for the PID controller
+ */
+    AgitatorSubsystem(
+        tap::Drivers* drivers,
+        tap::motor::MotorInterface& motor,
+        const AgitatorConfig& config,
+        const tap::algorithms::SmoothPidConfig& pidConfig);
+
+    void initialize() override;
+
+    void refresh() override;
+    void refreshSafeDisconnect() override;
+
+    /**
+     * Increments the desired position of the agitator
+     */
+    void moveToNextPosition();
+
+    /**
+     * @return The current position of the agitator in cummulative revolutions, unwrapped.
+     */
+    float getCurrentValue() const;
+
+    /**
+     * @return True if the agitator motor is online, false otherwise.
+     */
+    bool isOnline() const;
+
+private:
+    tap::motor::MotorInterface& motor;
+    AgitatorConfig config;
+    tap::algorithms::SmoothPid pidController;
+    float desiredPosition = 0.0f;
+    uint32_t lastRefreshTime = 0;
+};
+
+}  // namespace huskybot::control::agitator
